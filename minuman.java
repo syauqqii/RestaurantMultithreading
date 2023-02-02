8025718369e5e8470663c92d33b3727ae451e282
S Y A U Q I <57295427+syauqqii@users.noreply.github.com>
2023-02-02 22:49:55 +0700

    Create minuman.java

---
+++ minuman.java
@@ -0,0 +1,56 @@
+/* Dalam file ini kita membuat class minuman,
+ *  dengan menerapkan sebuah konsep inheritance,
+ *  dan juga menerapkan konsep polymorphism tentunya,
+ *  serta mendefinisikan isi dari sebuah function :
+ *
+ *  1. getNamaMenu() -> untuk return dari variable namaMenu
+ *  2. getHargaMenu() -> untuk return dari variable hargaMenu
+ *  3. getWaktuPembuatan() -> -> untuk return dari variable waktuPembuatan
+ */
+public class minuman<N, H, W> extends menu{
+    public minuman(N namaMenu, H hargaMenu, W waktuPembuatan){
+        this.namaMenu       = namaMenu;
+        this.hargaMenu      = hargaMenu;
+        this.waktuPembuatan = waktuPembuatan;
+    }
+    
+    @Override
+    N getNamaMenu(){
+        return (N)namaMenu;
+    }
+    
+    @Override
+    H getHargaMenu(){
+        return (H)hargaMenu;
+    }
+    
+    @Override
+    W getWaktuPembuatan(){
+        return (W)waktuPembuatan;
+    }
+    
+    /* Di dalam function ini kita membuat sebuah thread
+     *  untuk melakukan multi threading
+     */
+    public void proesePembuatan(){
+        try{
+            System.out.println(" $ Memulai prose pembuatan : "+namaMenu);
+            Thread.sleep((int)waktuPembuatan);
+            System.out.println(" > Proses pembuatan \""+namaMenu+"\" telah selesai!");
+        } catch(InterruptedException e){
+            /* try catch berfungsi untuk menangkap error,
+             *  di dalam case ini catch akan menangkap jenis error :
+             *  'InterruptedException' fungsinya agar tidak terjadi
+             *  crash saat running threading (multi processing) ini.
+             *
+             *  nah, jika kalian ingin menghandle sebuah error jenis ini,
+             *  dengan cara misalnya anda ingin kalau error program akan exit,
+             *  maka kalian bisa mengeksekusi dengan menambahkan code :
+             *  System.exite(0);
+             *
+             *  akan tetapi di case saya, saya membiarkannya saja hehehe jadi
+             *  saya kosongi tidak saya berikan sebuah code.
+             */
+        }
+    }
+}
